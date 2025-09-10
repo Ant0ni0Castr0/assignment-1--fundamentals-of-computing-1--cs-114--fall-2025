bde3aac7cd4a43b2cd2924e15c4a707974167baa
Ant0ni0Castr0 <antoniocastro2008@outlook.com>
2025-09-10 16:47:49 -0400

    Change #1: In program 1, I added 11 print functions that allow the code to print AC as an ASCII art. Each print function correlates to a different line os A's and C's that help make up the ASCII art.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,17 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    System.out.println("          AAA              CCCCCCCCCCCCC");
+    System.out.println("         AA AA           CCCC         CCCC");
+    System.out.println("        AA   AA         CC");
+    System.out.println("       AA     AA        CC");
+    System.out.println("      AA       AA       CC");
+    System.out.println("     AA         AA      CC");
+    System.out.println("    AA           AA     CC");
+    System.out.println("   AAAAAAAAAAAAAAAAA    CC");
+    System.out.println("  AA               AA   CC");
+    System.out.println(" AA                 AA   CCCC         CCCC");
+    System.out.println("AA                   AA    CCCCCCCCCCCCC");
+
 
   }
 }
