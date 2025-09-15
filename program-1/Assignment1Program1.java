c34a70c6b853df38e2f7abb639c0c9ad64b36291
Ant0ni0Castr0 <antoniocastro2008@outlook.com>
2025-09-15 11:21:07 -0400

    Change #2: added code that will ask the user for a 5 character string, and repeat until exactly 5 characters are entered, and then it removes the 1st and last characters and reverses the string. There is also code that asks to enter in a number in fahrenheit, which after taking user input converts the number to celsius, and then there is code to generate a random number, and then at the very end of the program, a print command combines the celsius number, the reversed character string, and the random number to make a new string.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,4 +1,8 @@
-public class Assignment1Program1 {
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Assignment1Program1 {  
   public static void main(String[] args) {
     System.out.println("          AAA              CCCCCCCCCCCCC");
     System.out.println("         AA AA           CCCC         CCCC");
@@ -12,6 +16,40 @@ public class Assignment1Program1 {
     System.out.println(" AA                 AA   CCCC         CCCC");
     System.out.println("AA                   AA    CCCCCCCCCCCCC");
 
+    System.out.println(""); // This will create space to tidy up the terminal
+    
+    //This section will loop until a user enters exactly 5 characters.
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
 
+    while (input.length() != 5){
+      System.out.println("Please enter in 5 characters");
+      input = scanner.nextLine();  // Reads a full line of text
+    }
+    
+    String Trimmed = input.substring(1, input.length() - 1);
+    String Reversed = new StringBuilder(Trimmed).reverse().toString();
+
+    System.out.println(""); // This will create space to tidy up the terminal
+
+    System.out.println("Enter in a number in fahrenheit");
+    double tempF = scanner.nextDouble(); // Reads a full line of text
+    double tempC = (tempF - 32) * 5 / 9;
+
+    System.out.println(""); // This will create space to tidy up the terminal
+
+
+    System.out.println("Generating a random number. Continuing...");
+    Random rand = new Random();
+    int min = 32;
+    int max = 16384;
+    int randomNum = rand.nextInt(max - min + 1) + min;
+
+    System.out.println(""); // This will create space to tidy up the terminal
+    
+    System.out.println("Your new string is " + tempC + Reversed + randomNum);
+
+
+    scanner.close();
   }
 }
